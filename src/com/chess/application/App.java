8704d8c171c3b4dd1a0d28bfc6c41962eb18858c
vjsb <joao.victorcoriolano@gmail.com>
2021-02-25 15:13:30 -0300

    First class: Position

---
+++ src/com/chess/application/App.java
@@ -0,0 +1,14 @@
+package com.chess.application;
+
+import com.chess.boardgame.Position;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
+
+	}
+
+}
