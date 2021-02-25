e87a837b24d369f71db32a80ddffa22ecf053b51
vjsb <joao.victorcoriolano@gmail.com>
2021-02-25 16:31:22 -0300

    BoardException and defensive programming

--- src/com/chess/boardgame/Board.java
+++ src/com/chess/boardgame/Board.java
@@ -11,7 +11,9 @@ public class Board {
 	 */
 	
 	public Board(Integer rows, Integer columns) {
-		
+		if (rows < 1 || columns < 1) {
+			throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -22,33 +24,58 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(Integer rows) {
-		this.rows = rows;
-	}
-
 	public Integer getColumns() {
 		return columns;
 	}
 
-	public void setColumns(Integer columns) {
-		this.columns = columns;
-	}
-	
 	//retorna a matriz pieces na linha row e na coluna column
 	public Piece piece(Integer row, Integer column) {
+		if (!positionExists(row, column)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[row][column];
 	}
 	
 	//recebe a peça pela posição
 	public Piece piece(Position position) {
+		//se a posição que deseja inserir não existe
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
 	//método para por a peça em uma posição
 	public void placePiece(Piece piece, Position position) {
+		//se existir uma peça ja na mesma posição
+		if (thereIsAPiece(position)) {
+			throw new BoardException("There is already a piece on position " + position);
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position; //coloca a posição recebendo posição ao invés de receber nula igual antes
 	}
+	
+	//metodo auxiliar, para ver se a posição existe medindo as medidas (rows, columns) do tabuleiro inteiro
+	private boolean positionExists(Integer row, Integer column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+	
+	//pega o metodo auxiliar e esta se uma posição existe
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	
+	//metodo para ver se ja existe uma peça
+	public boolean thereIsAPiece(Position position) {
+		if (thereIsAPiece(position)) {
+			throw new BoardException("There is already a piece on position " + position);
+		}
+		return piece(position) != null;
+	}
+	
+	
+	
+	
 }
 	
 	
